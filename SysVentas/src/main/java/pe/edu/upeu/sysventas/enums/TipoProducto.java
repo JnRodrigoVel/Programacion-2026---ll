3f2e197fc53f3547f5696c9a86092e3909dc7523
JnRodrigoVel <jeanpierovel@gmail.com>
2026-09-25 10:27:36 -0500

    Add files via upload

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/enums/TipoProducto.java
@@ -0,0 +1,20 @@
+package pe.edu.upeu.sysventas.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum TipoProducto {
+    PRODUCTO("Producto"),
+    PREPARADO("Preparado"),
+    SERVICIO("Servicio");
+
+    String descripcion;
+    TipoProducto(String descripcion){
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion;
+    }
+}
